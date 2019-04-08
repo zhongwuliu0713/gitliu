adcd262b6d0a41359c5887a338d2c753bca0d398
zhongwu_liu <zhongwu_liu@163.com>
2019-04-09 06:48:05 +0800

    commit to local git repository

---
+++ myproject/src/main/java/myproject/Example.java
@@ -0,0 +1,23 @@
+package myproject;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Example.class, args);
+//		SpringApplication app = new SpringApplication(Example.class);
+//		app.setBannerMode(Banner.Mode.OFF);
+//		app.run(args);
+	}
+}
